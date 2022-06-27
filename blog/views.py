f28be7337829f4097ff81c2b757cb720716aca23
Nastya_7 <saa_1299@mail.ru>
2022-06-27 22:51:29 +0300

    Исправление urls для отображения по url post_edit

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render, get_list_or_404
+from django.http import HttpResponse
+from django.shortcuts import render, get_list_or_404, redirect, get_object_or_404
+from django.views import View
+from django.views.generic import TemplateView, ListView
+
 from .models import Post
 from django.utils import timezone
 from .forms import PostForm
@@ -11,10 +15,46 @@ def post_list(request):
 
 
 def post_detail(request, pk):
-    post = get_list_or_404(Post, pk=pk)
+    post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
 
 def post_new(request):
-    form = PostForm()
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
+    # form = PostForm()
+    # return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+
+class PostListView(ListView):
+    template_name = 'blog/post_list.html'
+    model = Post
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(object_list=object_list, **kwargs)
+        context['posts'] = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+        return context
