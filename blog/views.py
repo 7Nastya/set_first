f7a6c00e8b3cacf986af4419d713ede374654a03
Nastya_7 <saa_1299@mail.ru>
2022-07-20 14:32:20 +0300

    Классы-представления PostList

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,8 @@
-from django.core.paginator import Paginator
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect, Http404
-from django.shortcuts import render, get_list_or_404, redirect, get_object_or_404
-from django.views import View
-from django.views.generic import TemplateView, ListView, DetailView
+from django.http import HttpResponse, HttpResponseNotFound
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
 from django.views.generic.edit import FormView, UpdateView
 from django.urls import reverse
-#10.07.22
-# from rest_framework.response import Response
-# from rest_framework.views import APIView
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm
 # 17.07.22
@@ -19,18 +14,34 @@ from .forms import PostForm
 menu = ["О сайте", "Обратная связь", "Войти", "Регистрация"]
 
 
+class PostList(ListView):
+    queryset = Post.objects.all()
+    context_object_name = 'post_list'
+    template_name = 'blog/post_list.html'
+    paginate_by = 4
 
-def post_list(request):
-   # task = supper_sum.delay(900, 100)
-    contact_list = Post.objects.all()
-    paginator = Paginator(contact_list, 4)
-    page_number = request.GET.get('page')
-    page = paginator.get_page(page_number)
-    return render(request, 'blog/post_list.html', {'page': page, 'posts': contact_list, 'menu': menu, 'title':'Главная страница'})
+    def get_paginate_by(self, queryset):
+        return self.paginate_by
 
-def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post, 'menu':menu, 'title':'Подробно'})
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['title'] = 'Главная страница'
+        context['menu'] = menu
+        return context
+
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+
+    def get_post(self, pk):
+        return get_object_or_404(Post, pk=pk)
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['title'] = 'Подробно'
+        context['menu'] = menu
+        return context
 
 class PostNew(FormView):
     form_class = PostForm
@@ -54,20 +65,8 @@ class PostNew(FormView):
         return self.object
 
     def get_success_url(self):
-        return reverse('post_detail',  kwargs={'pk': self.object.instance.pk } )
+        return reverse('post_detail', kwargs={'pk': self.object.instance.pk})
 
-# def post_new(request):
-#     if request.method == 'POST':
-#         form = PostForm(request.POST)
-#         if form.is_valid():
-#             post = form.save(commit=False)
-#             post.author = request.user
-#             post.published_date = timezone.now()
-#             post.save()
-#             return redirect('post_detail', pk=post.pk)
-#     else:
-#         form = PostForm()
-#     return render(request, 'blog/post_new.html', {'form': form, 'menu':menu, 'title':'Добавить статью'})
 
 class PostEdit(UpdateView):
     model = Post
@@ -80,7 +79,7 @@ class PostEdit(UpdateView):
 
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
-        context['title'] = 'Подробно'
+        context['title'] = 'Редактирование'
         context['menu'] = menu
         return context
 
@@ -97,22 +96,10 @@ class PostEdit(UpdateView):
     def get_success_url(self):
         return reverse('post_detail', kwargs={'pk': self.object.pk})
 
-# def post_edit(request, pk):
-#     post = get_object_or_404(Post, pk=pk)
-#     if request.method == 'POST':
-#         form = PostForm(request.POST, instance=post)
-#         if form.is_valid():
-#             post = form.save(commit=False)
-#             post.author = request.user
-#             post.published_date = timezone.now()
-#             post.save()
-#             return redirect('post_detail', pk=post.pk)
-#     else:
-#         form = PostForm(instance=post)
-#     return render(request, 'blog/post_edit.html', {'form': form, 'menu':menu, 'title':'Подробно'})
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
+
 
 def user_login(request):
     if request.method == 'POST':
@@ -131,10 +118,3 @@ def user_login(request):
     else:
         form = LoginForm()
     return render(request, 'blog/login.html', {'form': form})
-
-# class PostView(APIView):
-#     def get(self, request):
-#         post = Post.objects.all()
-#         return Response({"post":post})
-
-
